8e2e3b963a90c1e3bd3389ae075cc5533bff4ef4
Sol Maria Condori <smcsolmaria@gmail.com>
2023-02-27 12:07:29 -0400

    refactor: modificacion de localizador del titulo principal del contenido

--- src/main/ui/inmueblesUI/commonUI/CommonElemntsUI.java
+++ src/main/ui/inmueblesUI/commonUI/CommonElemntsUI.java
@@ -3,6 +3,7 @@ package main.ui.inmueblesUI.commonUI;
 import org.openqa.selenium.By;
 
 public class CommonElemntsUI {
-    public static By ttlTituloPrincipal = By.xpath("//div[@id='ventana']//h1");
+    //public static By ttlTituloPrincipal = By.xpath("//div[@id='ventana']//h1");
+    public static By ttlTituloPrincipal = By.xpath("//h1");
     public static By progressBar = By.id("fondo");
 }
